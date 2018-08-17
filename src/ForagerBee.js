4adc8ee62bd1b692febd3b0d5f8cc26ce57028be
atomicn50 <tinhle46@gmail.com>
2018-08-16 18:57:46 -0700

    finished 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
-};
+class ForagerBee extends HoneyMakerBee {
+  constructor(job) {
+    super();
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
